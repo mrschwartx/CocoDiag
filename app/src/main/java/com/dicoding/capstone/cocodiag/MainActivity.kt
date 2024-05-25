04a7ebe3bd41af1b6af6d81a27f65cce884a4dfe
sutanbaihaqi23 <116020557+sutanbaihaqi23@users.noreply.github.com>
2024-05-26 05:03:00 +0700

    trying implement UI design and navbar to MainActivity

--- app/src/main/java/com/dicoding/capstone/cocodiag/MainActivity.kt
+++ app/src/main/java/com/dicoding/capstone/cocodiag/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.dicoding.capstone.cocodiag
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
