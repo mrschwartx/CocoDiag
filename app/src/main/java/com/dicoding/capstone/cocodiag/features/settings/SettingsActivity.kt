1511d3d13650fbbc18a725d43b4c44b28b0b6b80
sutanbaihaqi23 <116020557+sutanbaihaqi23@users.noreply.github.com>
2024-05-27 04:18:56 +0700

    Create Setting activity , but it's still not responsive for landscape mode.'

---
+++ app/src/main/java/com/dicoding/capstone/cocodiag/features/settings/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.capstone.cocodiag.features.settings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.capstone.cocodiag.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
