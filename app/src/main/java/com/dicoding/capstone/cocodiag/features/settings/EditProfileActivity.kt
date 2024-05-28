54558db441d99d93ffb6399d9254198a8e267e28
sutanbaihaqi23 <116020557+sutanbaihaqi23@users.noreply.github.com>
2024-05-29 05:59:37 +0700

    modify setting activity and main for landscape mode +  create edit profil activity

--- app/src/main/java/com/dicoding/capstone/cocodiag/features/settings/EditProfileActivity.kt
+++ app/src/main/java/com/dicoding/capstone/cocodiag/features/settings/EditProfileActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.capstone.cocodiag.R
 import com.dicoding.capstone.cocodiag.common.setBottomNavBar
 import com.dicoding.capstone.cocodiag.databinding.ActivityEditProfileBinding
-import com.dicoding.capstone.cocodiag.databinding.ActivitySettingsBinding
+
 
 class EditProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditProfileBinding
@@ -13,6 +13,6 @@ class EditProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_edit_profile)
-        setBottomNavBar(this@EditProfileActivity, binding.bottomNavigation, R.id.nav_setting)
+
     }
 }
