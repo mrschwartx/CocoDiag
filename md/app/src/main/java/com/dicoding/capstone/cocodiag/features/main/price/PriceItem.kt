60235002de15f96995cd9d361211c79aa81b3eea
Agus <agussmkertjhaan@gmail.com>
2024-06-17 13:14:51 +0700

    fix(md/main): image slider

--- md/app/src/main/java/com/dicoding/capstone/cocodiag/features/main/price/PriceItem.kt
+++ md/app/src/main/java/com/dicoding/capstone/cocodiag/features/main/price/PriceItem.kt
@@ -3,4 +3,5 @@ package com.dicoding.capstone.cocodiag.features.main.price
 data class PriceItem(
     val date : String,
     val price : String,
-    val region : String)
+    val region : String
+)
